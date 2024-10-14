e794d4d3fc87db21ab2d85ee72ea11c602a7ce81
Krzysztof Kolodziej <krzysztof.kolodziej2212@gmail.com>
2024-10-14 12:16:49 +0200

    Initial commit

---
+++ src/main/java/com/shop/shop_order_service/ShopOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shop.shop_order_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopOrderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shop/shop_order_service/ShopOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.shop_order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
