110ecbe545fd8aaf595894aeca1a2b401fa8f75a
Krzysztof Kolodziej <krzysztof.kolodziej2212@gmail.com>
2024-10-21 13:55:07 +0200

    Add class Order
    to specify details user order.

---
+++ src/main/java/com/shop/shop_order_service/model/Order.java
@@ -0,0 +1,34 @@
+package com.shop.shop_order_service.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private Long userId;
+    @Column(nullable = false)
+    private LocalDateTime orderDate;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private OrderStatus status;
+    @Column(nullable = false)
+    private BigDecimal totalAmount;
+}
+
+
+
